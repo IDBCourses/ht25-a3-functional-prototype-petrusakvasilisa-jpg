4fbe390ec6da0cb632e3468341533b8807e75df0
petrusakvasilisa-jpg <petrusakvasilisa@gmail.com>
2025-11-03 01:13:48 +0100

    Created all objects that are going to appear on the screen (maze, star and finish point) using functions, arrays and a for loop, where the maze is centered in the middle of the screen. Star and finish point do not correspond to the maze position, therefore in preview page and in browser theur position is different.

--- Template/script.js
+++ Template/script.js
@@ -5,21 +5,89 @@
 
 import * as Util from "./util.js";
 
-// State variables are the parts of your program that change over time.
+const star = Util.thing; 
 
-// Settings variables should contain all of the "fixed" parts of your programs
+Util.setPosition(0.22,0.12,star); 
+
+function createMaze(){
+ 
+ const standartSize = Math.min(window.innerWidth, window.innerHeight);
+ const mazeScale = 1.75;
+ const walls = [
+  {x:0.25,y:0.48,w:0.51,h:0.01,rotation:0},
+  { x: 0.75, y: 0.48, w: 0.01, h: 0.472, rotation: 90 },
+  { x: 0.26, y: 0.95, w: 0.5, h: 0.01, rotation: 180 },
+  { x: 0.26, y: 0.56, w: 0.01, h: 0.4, rotation: 270 },
+  { x: 0.26, y: 0.55, w: 0.41, h: 0.01, rotation: 0 },
+  { x: 0.66, y: 0.551, w: 0.01, h: 0.34, rotation: 90 },
+  { x: 0.34, y: 0.891, w: 0.33, h: 0.01, rotation: 180 },
+  { x: 0.34, y: 0.60, w: 0.01, h: 0.3, rotation: 270 },
+  { x: 0.345, y: 0.60, w: 0.24, h: 0.01, rotation: 0 },
+  { x: 0.58, y: 0.60, w: 0.01, h: 0.24, rotation: 90 },
+  { x: 0.43, y: 0.83, w: 0.15, h: 0.01, rotation: 180 },
+  { x: 0.42, y: 0.67, w: 0.01, h: 0.17, rotation: 270 },
+  { x: 0.42, y: 0.67, w: 0.09, h:0.01, rotation: 0 },
+  { x: 0.5, y: 0.67, w: 0.01, h: 0.1, rotation: 90},
+
+ ];
+
+ const labyrinthWalls = [];
+
+ let minX = Math.min(...walls.map(w=>w.x));
+ let maxX = Math.max(...walls.map(w=>w.x));
+ let minY = Math.min(...walls.map(w=>w.y));
+ let maxY = Math.max(...walls.map(w=>w.y));
+
+ const mazeWidth = (maxX-minX)*standartSize*mazeScale;
+ const mazeHeight = (maxY-minY)*standartSize*mazeScale;
+ const shiftX = window.innerWidth/2-mazeWidth/2-minX*standartSize*mazeScale;
+ const shiftY = window.innerHeight/2-mazeWidth/2-minY*standartSize*mazeScale;
+
+ for(const wall of walls){
+  const w = Util.createThing();
+  
+  Util.setSize(wall.w * standartSize*mazeScale, wall.h * standartSize*mazeScale, w);
+  Util.setPositionPixels(wall.x * standartSize*mazeScale+shiftX, wall.y * standartSize*mazeScale+shiftY, w);
+  Util.setRoundedness(0,w);
+  Util.setColour(255,255,255,1,w);
+
+  labyrinthWalls.push(w);
+
+ };
+ return labyrinthWalls;
+};
+
+function createFinish() {
+  const finish = Util.createThing();
+  const finishSize = 0.08;
+  const standartSize = Math.min(window.innerWidth,window.innerHeight);
+
+  Util.setSize(finishSize*standartSize,finishSize*standartSize,finish);
+  Util.setRoundedness(0.3,finish)
+  Util.setPosition(window.innerWidth/2, window.innerHeight/2,finish);
+  Util.setColour(255,255,2,1,finish);
+  Util.setPosition(0.447,0.43,finish);
+  return finish;
+};
+
 
 // Code that runs over and over again
 function loop() {
+ window.requestAnimationFrame(loop);
 
-  window.requestAnimationFrame(loop);
-}
+};
 
 // Setup is run once, at the start of the program. It sets everything up for us!
 function setup() {
-  // Put your event listener code here
+ createFinish();
+ createMaze();
+
+  
+  window.requestAnimationFrame(loop);
+ 
+ // Put your event listener code here
 
   window.requestAnimationFrame(loop);
-}
+};
 
 setup(); // Always remember to call setup()!
